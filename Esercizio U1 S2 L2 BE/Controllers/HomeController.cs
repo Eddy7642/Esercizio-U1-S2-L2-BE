e03c42e377846c05663b1188f82adb19bd62cc72
Luca Chisari <75541078+Eddy7642@users.noreply.github.com>
2024-06-25 15:48:07 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio U1 S2 L2 BE/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Esercizio_U1_S2_L2_BE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Esercizio_U1_S2_L2_BE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
